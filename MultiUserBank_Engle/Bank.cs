cae6550a674c1ef3c3d6e2120b4149aea4b72c70
Ian Engle <ian.engle@student.centralia.edu>
2024-05-18 23:43:54 -0700

    Added started project to the repo

---
+++ MultiUserBank_Engle/Bank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUserBank_Engle
+{
+    public class Bank
+    {
+        private decimal _vault = 10000;
+        private string[,] _users = new string[4, 3]
+        {
+            {"jlennon", "johnny", "1250" },
+            {"pmccartney", "pauly", "2500" },
+            {"gharrison", "georgy", "3000" },
+            {"rstarr", "ringoy", "1001"}
+        };
+        public decimal BankBalance
+        {
+            get { return _vault; }
+        }
+
+    }
+}
---
+++ MultiUserBank_Engle/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiUserBank_Engle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank MultiUserBank = new Bank();
+
+        }
+    }
+}
