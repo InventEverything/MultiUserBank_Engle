4a4817257207117f93bbcb9cf531e5a6f9586eef
Ian Engle <ian.engle@student.centralia.edu>
2024-05-20 05:56:24 -0700

    Most everything is implemented. Error checking and validating that all requirements are met still needed.

--- MultiUserBank_Engle/Bank.cs
+++ MultiUserBank_Engle/Bank.cs
@@ -9,6 +9,7 @@ namespace MultiUserBank_Engle
     public class Bank
     {
         private decimal _vault = 10000;
+        private int _loggedIn=5;
         private string[,] _users = new string[4, 3]
         {
             {"jlennon", "johnny", "1250" },
@@ -16,10 +17,55 @@ namespace MultiUserBank_Engle
             {"gharrison", "georgy", "3000" },
             {"rstarr", "ringoy", "1001"}
         };
+        public string LogIn(string username, string password)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (username == _users[i, 0] && password == _users[i, 1])
+                {
+                    _loggedIn = i;
+                    return username;
+                }
+            }
+            _loggedIn=5;
+            return "Invalid credentials\n";
+        }
+        public string Balance
+        {
+            get { return "Remaining balance: " + decimal.Parse(_users[_loggedIn, 2]).ToString("c") + "\n"; }
+        }
+        public string Deposit(string value)
+        {
+            _users[_loggedIn, 2] = (decimal.Parse(_users[_loggedIn, 2]) + decimal.Parse(value)).ToString();
+            _vault += decimal.Parse(value);
+            return ("Successfully deposited " + decimal.Parse(value).ToString("c") + "\n");
+        }
+        public string Withdraw(string value)
+        {
+            string capped = string.Empty;
+            if (decimal.Parse(value) > 500m)
+            {
+                value = "500";
+                capped = "Withdraw limit $500.\n";
+            }
+            if(decimal.Parse(value) > decimal.Parse(_users[_loggedIn, 2]))
+            {
+                value = _users[_loggedIn, 2];
+                capped += "Withdraw exceeds reamaining balance.\n";
+            }
+            _users[_loggedIn, 2] = (decimal.Parse(_users[_loggedIn, 2]) - decimal.Parse(value)).ToString();
+            _vault -= decimal.Parse(value);
+            return capped + "You successfully withdrew " + decimal.Parse(value).ToString("c") + "\n";
+        }
+
+
+        public string LoggedIn()
+        {
+            return _users[_loggedIn, 0];
+        }
         public decimal BankBalance
         {
             get { return _vault; }
         }
-
     }
 }
--- MultiUserBank_Engle/Program.cs
+++ MultiUserBank_Engle/Program.cs
@@ -1,11 +1,84 @@
-﻿namespace MultiUserBank_Engle
+﻿using System.Runtime.CompilerServices;
+using System.Security.Principal;
+
+namespace MultiUserBank_Engle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Bank MultiUserBank = new Bank();
+            Bank BigPockets = new Bank();
+            string User;
+            string PW;
+            Console.WriteLine("Bank cash on hand: " + BigPockets.BankBalance.ToString("C") + "\n");
+            do
+            {
+                User = LogOn("ID");
+                PW = LogOn("PW");
+                if (BigPockets.LogIn(User, PW) == User)
+                {
+                    Console.Clear();
+                    Welcome(BigPockets);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine(BigPockets.LogIn(User, PW));
+                }
+            } while (true);
 
+        }
+        static string LogOn(string validation)
+        {
+            Console.Write("What is your " + validation + "?\n" + validation + ": ");
+            return Console.ReadLine();
+        }
+        static void Welcome(Bank Account)
+        {
+            ConsoleKeyInfo selection;
+            Console.WriteLine("Welcome " + Account.LoggedIn() + ".");
+            do {
+                Console.WriteLine("\nWhat would you like to do?\n\n(1) Check balance\n(2) Deposit\n(3) Withdraw\n(4) Logging out");
+                selection = Console.ReadKey();
+                //Check balance
+                if (selection.Key == ConsoleKey.D1 || selection.Key == ConsoleKey.NumPad1)
+                {
+                    Console.Clear();
+                    Console.WriteLine(Account.Balance);
+                }
+                //Deposit
+                else if (selection.Key == ConsoleKey.D2 || selection.Key == ConsoleKey.NumPad2)
+                {
+                    Console.Clear();
+                    Console.Write("How much would you like to deposit?\n$");
+                    string amount = Console.ReadLine();
+                    Console.Clear();
+                    Console.Write(Account.Deposit(amount));
+                    Console.WriteLine(Account.Balance);
+                }
+                //Withdraw
+                else if (selection.Key == ConsoleKey.D3 || selection.Key == ConsoleKey.NumPad3)
+                {
+                    Console.Clear();
+                    Console.Write("How much would you like to withdraw?\n$");
+                    string amount = Console.ReadLine();
+                    Console.Clear();
+                    Console.Write(Account.Withdraw(amount));
+                    Console.WriteLine(Account.Balance);
+                }
+                //Log out
+                else if (selection.Key == ConsoleKey.D4 || selection.Key == ConsoleKey.NumPad4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Bank cash on hand: " + Account.BankBalance.ToString("C")+"\n");
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input");
+                }
+            }while (true);
         }
     }
 }
